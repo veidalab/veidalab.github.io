dacc25b0575d7a206baacfd37d41b1a2c60d99aa
Brad Stimpson <brad@synaptop.com>
2017-04-02 21:25:46 -0400

    updated for lambda contactform

--- js/_main.js
+++ js/_main.js
@@ -28,6 +28,41 @@ $(document).ready(function(){
 	$('#main').fitVids();
 });
 
+$(document).ready(function(){
+
+    // Setup the form to watch for the submit event
+    $('#myForm').submit(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        console.log(this.action);
+
+        // Grab the elements from the form to make up
+        // an object containing name, email and message
+        var data = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            message: document.getElementById('message').value
+        };
+
+        $.ajax({
+            type: "POST",
+            url: this.action,
+            data: data,
+            success: function(data) {
+                window.location.href = "/thanks/";
+            },
+            dataType: "json",
+            error: function (xhr, ajaxOptions, thrownError) {
+                var errorMessage = 'Error! Email not sent!';
+                if (thrownError == 'Bad Request') {
+                    errorMessage = xhr.responseJSON['Message'].split(': ')[1];
+                }
+                $('#response').html(errorMessage).addClass('error').fadeIn('fast');
+            }
+        });
+    });
+});
+
 // Table of Contents title. Change text to localize
 $('#markdown-toc').prepend('<li><h6>{{ site.data.messages.locales[site.locale].overview }}</h6></li>');
 
