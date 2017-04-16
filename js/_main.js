034a9bc0c08beca17921db876dc2356befe8dca1
Brad Stimpson <brad@synaptop.com>
2017-04-16 12:01:53 -0400

    semi-working subscribe/contact form

--- js/_main.js
+++ js/_main.js
@@ -44,13 +44,19 @@ $(document).ready(function(){
             message: document.getElementById('message').value
         };
 
+        if(document.getElementById('subscribecheck').checked) {
+            $("#txtAge").show();
+        } else {
+            $("#txtAge").hide();
+        }
+
         $.ajax({
             type: "POST",
             url: this.action,
             data: JSON.stringify(data),
             success: function(data) {
                 //window.location.href = "/thanks/";
-                $('#response').html("Message sent!").addClass('success').fadeIn('fast');
+                $('#msg-response').html("Message sent!").addClass('success').fadeIn('fast');
             },
             dataType: "json",
             contentType: "application/json",
@@ -59,7 +65,39 @@ $(document).ready(function(){
                 if (thrownError == 'Bad Request') {
                     errorMessage = xhr.responseJSON['Message'].split(': ')[1];
                 }
-                $('#response').html(errorMessage).addClass('error').fadeIn('fast');
+                $('#msg-response').html(errorMessage).addClass('error').fadeIn('fast');
+            }
+        });
+    });
+
+    // Setup the form to watch for the submit event
+    $('#subscribeForm').submit(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        console.log(this.action);
+
+//        Grab the elements from the form to make up
+//        an object containing name, email and message
+        var data = {
+            email: document.getElementById('subscribe_email').value
+        };
+
+        $.ajax({
+            type: "POST",
+            url: this.action,
+            data: JSON.stringify(data),
+            success: function(data) {
+                //window.location.href = "/thanks/";
+                $('#subscribe_response').html("Message sent!").addClass('success').fadeIn('fast');
+            },
+            dataType: "json",
+            contentType: "application/json",
+            error: function (xhr, ajaxOptions, thrownError) {
+                var errorMessage = 'Error! Email not sent!';
+                if (thrownError == 'Bad Request') {
+                    errorMessage = xhr.responseJSON['Message'].split(': ')[1];
+                }
+                $('#subscribe_response').html(errorMessage).addClass('error').fadeIn('fast');
             }
         });
     });
