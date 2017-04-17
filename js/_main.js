84230dc79459b9afd96bbfbc872f7b8530af38d2
Brad Stimpson <brad@synaptop.com>
2017-04-16 22:45:03 -0400

    added header particles and updated google analytics

--- js/_main.js
+++ js/_main.js
@@ -1,5 +1,5 @@
 // Initialise FlexSlider for Carousels
-$(document).ready(function() {
+$(document).ready(function () {
     $('.flexslider').flexslider({
         animation: 'fade',
         controlNav: false,
@@ -11,27 +11,27 @@ $(document).ready(function() {
 });
 
 // Off Canvas Sliding
-$(document).ready(function(){
-  // Menu button click
-  $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function(e){
-    // $('#js-body').toggleClass('no-scroll');
-    $('#js-menu, #js-menu-screen').toggleClass('is-visible');
-    $('#js-menu-trigger').toggleClass('slide close');
-    // $('#masthead, #page-wrapper').toggleClass('slide');
-    e.preventDefault();
-  });
+$(document).ready(function () {
+    // Menu button click
+    $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function (e) {
+        // $('#js-body').toggleClass('no-scroll');
+        $('#js-menu, #js-menu-screen').toggleClass('is-visible');
+        $('#js-menu-trigger').toggleClass('slide close');
+        // $('#masthead, #page-wrapper').toggleClass('slide');
+        e.preventDefault();
+    });
 });
 
 // FitVids
-$(document).ready(function(){
-	// Target your .container, .wrapper, .post, etc.
-	$('#main').fitVids();
+$(document).ready(function () {
+    // Target your .container, .wrapper, .post, etc.
+    $('#main').fitVids();
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     // Setup the form to watch for the submit event
-    $('#myForm').submit(function(e){
+    $('#myForm').submit(function (e) {
         e.preventDefault();
         e.stopPropagation();
         console.log(this.action);
@@ -44,19 +44,19 @@ $(document).ready(function(){
             message: document.getElementById('message').value
         };
 
-        if(document.getElementById('subscribecheck').checked) {
-            $("#txtAge").show();
-        } else {
-            $("#txtAge").hide();
+        if (document.getElementById('subscribecheck').checked) {
+            $('#subscribe_email').val(data.email);
+            $('#subscribeForm').submit();
         }
 
         $.ajax({
             type: "POST",
             url: this.action,
             data: JSON.stringify(data),
-            success: function(data) {
+            success: function (data) {
                 //window.location.href = "/thanks/";
                 $('#msg-response').html("Message sent!").addClass('success').fadeIn('fast');
+                ga('send', 'pageview', '/goals/contact-us');
             },
             dataType: "json",
             contentType: "application/json",
@@ -71,7 +71,7 @@ $(document).ready(function(){
     });
 
     // Setup the form to watch for the submit event
-    $('#subscribeForm').submit(function(e){
+    $('#subscribeForm').submit(function (e) {
         e.preventDefault();
         e.stopPropagation();
         console.log(this.action);
@@ -86,9 +86,10 @@ $(document).ready(function(){
             type: "POST",
             url: this.action,
             data: JSON.stringify(data),
-            success: function(data) {
+            success: function (data) {
                 //window.location.href = "/thanks/";
                 $('#subscribe_response').html("Message sent!").addClass('success').fadeIn('fast');
+                ga('send', 'pageview', '/goals/subscribe');
             },
             dataType: "json",
             contentType: "application/json",
@@ -106,4 +107,54 @@ $(document).ready(function(){
 // Table of Contents title. Change text to localize
 $('#markdown-toc').prepend('<li><h6>{{ site.data.messages.locales[site.locale].overview }}</h6></li>');
 
-
+particlesJS("particles-js", {
+    "particles": {
+        "number": {"value": 100, "density": {"enable": true, "value_area": 800}},
+        "color": {"value": "#FFFFFF"},
+        "shape": {
+            "type": "circle",
+            "stroke": {"width": 0, "color": "#FFFFFF"},
+            "polygon": {"nb_sides": 5},
+            "image": {"src": "img/github.svg", "width": 100, "height": 100}
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {"enable": false, "speed": 1, "opacity_min": 0.1, "sync": false}
+        },
+        "size": {"value": 3, "random": true, "anim": {"enable": false, "speed": 40, "size_min": 0.1, "sync": false}},
+        "line_linked": {
+            "enable": true,
+            "distance": 157.82952832645452,
+            "color": "#FFFFFF",
+            "opacity": 0.25252724532232723,
+            "width": 0.7891476416322727
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {"enable": false, "rotateX": 600, "rotateY": 1200}
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {"enable": true, "mode": "repulse"},
+            "onclick": {"enable": true, "mode": "push"},
+            "resize": true
+        },
+        "modes": {
+            "grab": {"distance": 400, "line_linked": {"opacity": 1}},
+            "bubble": {"distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3},
+            "repulse": {"distance": 200, "duration": 0.4},
+            "push": {"particles_nb": 4},
+            "remove": {"particles_nb": 2}
+        }
+    },
+    "retina_detect": true
+});
